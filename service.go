df46a42cbda90c1f24451750669bb076ce58b183
Scott Newton <scott.newton@ft.com>
2017-03-10 14:36:46 +0000

    v5

--- service.go
+++ service.go
@@ -23,7 +23,7 @@ func (s service) forceImport(rw http.ResponseWriter, req *http.Request) {
 
 func (s service) Fetch() {
 	if len([]factsetResource{}) == 0 {
-		log.Warnf("Resource list not set; skipping run")
+		log.Warnf("Resource list not set, skipping run")
 		return
 	}
 	res := s.files
